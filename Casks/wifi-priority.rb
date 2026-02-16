046ea884d58bef11568311cbacc8b4e999012ebf
dhruv-anand-aintech <dhruv.anand@ainorthstartech.com>
2026-02-16 12:37:57 +0530

    Fix cask: update SHA256 for root-level app bundle zip

--- Casks/wifi-priority.rb
+++ Casks/wifi-priority.rb
@@ -1,6 +1,6 @@
 cask "wifi-priority" do
   version "0.5.0"
-  sha256 "33fe66c687b0b9bd68875e051dc3993494a49d5b451b8c412f4dc212e6a3e1dc"
+  sha256 "9b9c772f7fac67073b9b3c8235d4a6b359218262c94d67c96ec21bfe2c6ed397"
 
   url "https://github.com/dhruv-anand-aintech/wifi-priority-tui/releases/download/v#{version}/WiFiPrioritySwiftUI-#{version}.zip"
   name "WiFi Priority"
